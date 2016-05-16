870775599d4574ee2e291b11ae0ae63f016be606
katieO <katajello@yahoo.com>
2016-05-16 16:57:09 -0700

    player move appears on board and wont allow space to be played twice

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var box1Sticks = []
 var horizontalSticks = $('.horizontal')
 var verticalSticks = $('.vertical')
+var boxes = $('.box')
 var counter = 0
-function switchTurnes() {
+function switchTurns() {
   if (counter %2 == 0) {
     console.log("player 1")
   } else {
@@ -10,17 +11,47 @@ function switchTurnes() {
   }
 }
 
-
+boxes.on('click',function () {
+  console.log($(this))
+})
 
 horizontalSticks.on('click', function() {
-console.log($(this).hasClass('box1'))
-counter ++
-switchTurnes()
+  //console.log("this.getAttribute('style')",this.getAttribute('style'));
+  if (this.getAttribute('style') != 'background-color: red;') {
+  //console.log('red');
+    $(this).css('background-color', 'red')
+    counter ++
+    switchTurns()
+  }else {
+    console.log("click somewhere else ");
+  }
 })
 
 verticalSticks.on('click', function(){
-  console.log($(this).hasClass('box1'));
-  counter ++
-switchTurnes()
-
+  if (this.getAttribute('style') != 'background-color: red;') {
+    $(this).css('background-color', 'red')
+    counter ++
+    switchTurns()
+  }else {
+    console.log("click somewhere else ");
+  }
 })
+
+
+
+// how to switch colors between turns
+// verticalSticks.on('click', function(){
+//   if (this.getAttribute('style') != 'background-color: red;' || this.getAttribute('style') != 'background-color: blue;') {
+//     if (counter % 2 == 0) {
+//       $(this).css('background-color', 'red')
+//     }else {
+//       $(this).css('background-color', 'blue')
+//     }
+//     counter ++
+//     switchTurns()
+//   }else {
+//     console.log("click somewhere else fool");
+//   }
+//
+//
+// })

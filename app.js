798f9acf7c86b204ed5d7aaff0b21a08aaf54d7d
katieO <katajello@yahoo.com>
2016-05-17 15:26:31 -0700

    removed redundant code

--- app.js
+++ app.js
@@ -8,63 +8,48 @@ var game = {
   boxes: [[],[],[], []]
 }
 
-
-// var box1Array = []
-// var box2Array = []
-// var box3Array = []
-// var box4Array = []
-// var box1 = $('#box1')
-// var box2 = $('#box2')
-// var box3 = $('#box3')
-// var box4 = $('#box4')
 var horizontalSticks = $('.horizontal')
 var verticalSticks = $('.vertical')
-// var boxes = $('.box')
+
 var counter = 0
 var currentPlayer
 
 // determines current player and switches turns
-function switchTurns() {
+// function switchTurns() {
+//   if (counter %2 == 0) {
+//     console.log("player 1")
+//     currentPlayer = game.playe2
+//   } else {
+//     console.log("player 2");
+//     currentPlayer = game.player1
+//   }
+// }
+function whichPlayer() {
   if (counter %2 == 0) {
     console.log("player 1")
-    currentPlayer = game.player1
+    return game.player1
   } else {
     console.log("player 2");
-    currentPlayer = game.player2
+    return game.player2
   }
 }
-
-
-
-horizontalSticks.on('click', function(){
-  console.log(this.classList[1].split('-'));
-
-  var theBoxes = this.classList[1].split('-');
-    for (var i=1; i<theBoxes.length; i++){
-     game.boxes[theBoxes[i]].push('something');
-    }
-      $(this).off('click');
-})
-verticalSticks.on('click', function(){
-  console.log(this.classList[1].split('-'));
-
-  var theBoxes = this.classList[1].split('-');
-    for (var i=1; i<theBoxes.length; i++){
-     game.boxes[theBoxes[i]].push('something');
-    }
-      $(this).off('click');
-})
 // adds event listener to each border in the game field
 var sticks = function (stick) {
   stick.on('click', function () {
     if (this.getAttribute('style') != 'background-color: red;') {
       console.log($(this).attr('class'));
+      console.log(this.classList[1].split('-'));
+      var theBoxes = this.classList[1].split('-');
+        for (var i=1; i<theBoxes.length; i++){
+         game.boxes[theBoxes[i]].push('something');
+        }
+          $(this).off('click');
 // need function that will add clicked class items to clas array
 
 // check array length if .legth = 4 then fill box with currentplayer marker and add 1 to counter to continue turn
       $(this).css('background-color', 'red')
       counter ++
-      switchTurns()
+      // switchTurns()
 // prevents clicking an already used border
     }else {
       console.log("click somewhere else ");
@@ -76,12 +61,18 @@ sticks(verticalSticks)
 
 
 
-// console.log($('.box1'));
-// // loop that checks how many borders of each box is filled red
-// function checkBox (){
-//   for (var i = 0; i < $('.box1').length; i++) {
-//   if($('.box1').getAttribute('style')) {
-//
+// loop that checks how many borders of each box is filled red
+function checkBox (x){
+  for (var i = 0; i < game.boxes.length; i++) {
+  if (game.boxes[i].length == 4) {
+    if (whichPlayer == game.player1){
+      x.setAttribute('style', 'background-color: grey')
+    }else {
+      x.setAttribute('style', 'background-color: pink')
+    }
+  }
+}
+}
 // }
 //   }
 // }

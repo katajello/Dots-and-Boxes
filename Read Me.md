63161850ac43445a331d53c841da1469ba0253a1
katieO <katajello@yahoo.com>
2016-05-17 11:45:18 -0700

    push clicks to array

---
+++ Read Me.md
@@ -0,0 +1,39 @@
+Read Me
+
+Dots and Boxes
+
+Object: The player who paints the board with the most amount of squares is the winner. 
+
+![](https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Dots-and-boxes.svg/600px-Dots-and-boxes.svg.png)
+
+
+
+How to Play:
+
+* Select the size game you and a friend want to play
+* Each player takes turns drawing lines between two dots
+* Strategically its best to avoid adding a third side to any box
+*  Once a box can be closed (all 4 box borders are drawn) the player who draws the final border fills the box and gains a point also maintaining their turn. 
+*  play continues until all boxes are filled and all the game borders are drawn
+
+
+User Stories:
+
+* As a player, I want to know which players turn it is
+* As a player, I want to know which color pertains to my marker
+* As a player, I want to see my current score as the game progresses 
+
+Technologies:
+
+* HTML
+* CSS
+* JavaScript
+* Jquery
+
+Future Implementation:
+
+* sdajkasda
+* lasdajda
+
+
+You can play the game here: Link
--- app.js
+++ app.js
@@ -8,16 +8,21 @@ var game = {
 }
 
 
-var box1 = []
-var box2 = []
-var box3 = []
-var box4 = []
+var box1Array = []
+var box2Array = []
+var box3Array = []
+var box4Array = []
+var box1 = $('#box1')
+var box2 = $('#box2')
+var box3 = $('#box3')
+var box4 = $('#box4')
 var horizontalSticks = $('.horizontal')
 var verticalSticks = $('.vertical')
 var boxes = $('.box')
 var counter = 0
 var currentPlayer
 
+// determines current player and switches turns
 function switchTurns() {
   if (counter %2 == 0) {
     console.log("player 1")
@@ -28,13 +33,17 @@ function switchTurns() {
   }
 }
 
+// adds event listener to each border in the game field
 var sticks = function (stick) {
   stick.on('click', function () {
     if (this.getAttribute('style') != 'background-color: red;') {
-    //console.log('red');
+      console.log($(this).attr('class'));
+// need function that will add clicked class items to clas array
+// check array length if .legth = 4 then fill box with currentplayer marker and add 1 to counter to continue turn
       $(this).css('background-color', 'red')
       counter ++
       switchTurns()
+// prevents clicking an already used border
     }else {
       console.log("click somewhere else ");
     }
@@ -42,17 +51,76 @@ var sticks = function (stick) {
 }
 sticks(horizontalSticks)
 sticks(verticalSticks)
+console.log($('.box1'));
+// loop that checks how many borders of each box is filled red
+function checkBox (){
+  for (var i = 0; i < $('.box1').length; i++) {
+  if($('.box1').getAttribute('style')) {
 
-boxes.on('click',function () {
-  console.log($(this))
-})
-
-
-function getBox(box) {
-  if ($('#' + box))  {
-
+}
   }
 }
+
+// count filled in boxes
+
+
+for(var i = 0; i < 8; i += 1) {
+		$('body').append('<div class="horizontal box1 box3"></div>')
+	}
+	$('.box').on('click', function(){
+		var classNames = $(this).attr("class").toString().split(' ');
+		console.log(classNames)
+
+		// adding classes to proper arrays
+		classNames.forEach(function(className) {
+			if(className == 'box1') {
+				box1Array.push(className)
+			} else if(className == 'box2'){
+				box2Array.push(className)
+			}
+		})
+
+		if(box1Array.length >= 4) {
+			console.log("Box made!!! Do something.")
+		}
+
+
+		// remove the event listener
+		$(this).off()
+	})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// box1.push()
+
+
+
+
+
+
+
+
+// function getBox(box) {
+//   if ($('#' + box))  {
+//
+//   }
+// }
 
 // function check(a, b, c, d, player) {
 //       if (a.getAttribute('style')) == move && getId(b) == move && getId(c) == move && getId(d) == move) {

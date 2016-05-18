9a42ecc1fcdd35b23902ff3a25236038b0411c9e
katieO <katajello@yahoo.com>
2016-05-17 19:00:31 -0700

    complete box function added

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+
+// game object
 var game = {
   player1: {
-    points: 0
+    points: 0,
+    color: 'blue'
   },
   player2: {
-    points: 0
+    points: 0,
+    color: 'green'
   },
   boxes: [[],[],[], []]
 }
@@ -12,9 +16,9 @@ var horizontalSticks = $('.horizontal')
 var verticalSticks = $('.vertical')
 
 var counter = 0
-var currentPlayer
 
 // determines current player and switches turns
+// var currentPlayer
 // function switchTurns() {
 //   if (counter %2 == 0) {
 //     console.log("player 1")
@@ -24,6 +28,7 @@ var currentPlayer
 //     currentPlayer = game.player1
 //   }
 // }
+// tracks current players turn
 function whichPlayer() {
   if (counter %2 == 0) {
     console.log("player 1")
@@ -35,58 +40,28 @@ function whichPlayer() {
 }
 // adds event listener to each border in the game field
 var sticks = function (stick) {
+  // on click will change background color and push box class to an array
   stick.on('click', function () {
-    if (this.getAttribute('style') != 'background-color: red;') {
-      console.log($(this).attr('class'));
-      console.log(this.classList[1].split('-'));
+    if (this.getAttribute('style') != 'background-color: black;') {
+      // console.log($(this).attr('class'));
+      $(this).css('backgroundColor','black')
+      // console.log(this.classList[1].split('-'));
       var theBoxes = this.classList[1].split('-');
         for (var i=1; i<theBoxes.length; i++){
-         game.boxes[theBoxes[i]].push('something');
-        }
-          $(this).off('click');
-// need function that will add clicked class items to clas array
-
-// check array length if .legth = 4 then fill box with currentplayer marker and add 1 to counter to continue turn
-      $(this).css('background-color', 'red')
+         game.boxes[theBoxes[i]].push('stkclk');
+        //  console.log(game.boxes[theBoxes[i]], theBoxes[i])
+    if (game.boxes[theBoxes[i]].length > 3) {
+      $('#box' + theBoxes[i]).css('background-color', whichPlayer().color)
+      whichPlayer().points +=1
       counter ++
-      // switchTurns()
-// prevents clicking an already used border
-    }else {
-      console.log("click somewhere else ");
+    } console.log(whichPlayer().points);
+      console.log(counter);
+        }
+        // turns off click event
+        $(this).off('click');
+        counter ++
     }
   })
 }
 sticks(horizontalSticks)
 sticks(verticalSticks)
-
-
-
-// loop that checks how many borders of each box is filled red
-function checkBox (x){
-  for (var i = 0; i < game.boxes.length; i++) {
-  if (game.boxes[i].length == 4) {
-    if (whichPlayer == game.player1){
-      x.setAttribute('style', 'background-color: grey')
-    }else {
-      x.setAttribute('style', 'background-color: pink')
-    }
-  }
-}
-}
-// }
-//   }
-// }
-
-// count filled in boxes
-
-
-// 		if(box1Array.length >= 4) {
-// 			console.log("Box made!!! Do something.")
-// 		}
-//
-//
-// 		// remove the event listener
-// 	// 	$(this).off()
-// 	})
-//
-// // box1.push()
